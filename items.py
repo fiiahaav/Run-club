dd948904b20b26a90c53a95745a317a210120fab
Fiia Haavisto <fiiahaav@example.com>
2025-06-15 23:22:39 +0300

    Refactor registration/login

--- items.py
+++ items.py
@@ -1,7 +1,7 @@
 import db
 
 def add_item(title, description, run_length, user_id):
-    sql = """INSERT INTO items (title,      description, run_length, user_id) VALUES (?, ?, ?, ?)"""
+    sql = """INSERT INTO items (title, description, run_length, user_id) VALUES (?, ?, ?, ?)"""
     db.execute(sql, [title, description, run_length, user_id])
 
 def get_items():
