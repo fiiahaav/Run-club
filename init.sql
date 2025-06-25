6cabed285815c3947f1bda46284acad793430977
Fiia Haavisto <fiiahaav@example.com>
2025-06-25 20:52:27 +0300

    Add init.sql

---
+++ init.sql
@@ -0,0 +1,12 @@
+DELETE FROM classes;
+
+INSERT INTO classes (title, value) VALUES ('Juoksun tyyppi', 'kuntojuoksu');
+INSERT INTO classes (title, value) VALUES ('Juoksun tyyppi', 'intervallilenkki');
+INSERT INTO classes (title, value) VALUES ('Juoksun tyyppi', 'mäkitreeni');
+INSERT INTO classes (title, value) VALUES ('Juoksun tyyppi', 'maastojuoksu');
+INSERT INTO classes (title, value) VALUES ('Juoksun tyyppi', 'tekniikkaharjoitus');
+INSERT INTO classes (title, value) VALUES ('Juoksun tyyppi', 'ryhmäjuoksu');
+
+INSERT INTO classes (title, value) VALUES ('Juoksijan taso', 'aloittelija');
+INSERT INTO classes (title, value) VALUES ('Juoksijan taso', 'keskitaso');
+INSERT INTO classes (title, value) VALUES ('Juoksijan taso', 'kokenut');
